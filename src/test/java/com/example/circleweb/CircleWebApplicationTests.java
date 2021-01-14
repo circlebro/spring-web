ad7342e3ac15e719a5f052223fc67eb27f93dd36
circle <circle@mz.co.kr>
2021-01-14 14:54:03 +0900

    first commit

---
+++ src/test/java/com/example/circleweb/CircleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
